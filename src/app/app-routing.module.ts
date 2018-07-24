7fe11025facb233c0999caf0cedb392102b01411
Dhondi Srikant <dhondi.srikanth@gmail.com>
2018-07-24 09:57:37 +0530

    First commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { FullComponent } from './layouts/full/full.component';
+import { BlankComponent } from './layouts/blank/blank.component';
+
+export const Approutes: Routes = [
+  {
+    path: '',
+    component: FullComponent,
+    children: [
+      { path: '', redirectTo: '/starter', pathMatch: 'full' },
+      {
+        path: 'starter',
+        loadChildren: './starter/starter.module#StarterModule'
+      },
+      {
+        path: 'component',
+        loadChildren: './component/component.module#ComponentsModule'
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: '/starter'
+  }
+];
